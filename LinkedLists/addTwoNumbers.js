7f67ec28afaf63f0cea55e0d2a7059261582f12c
asim samuel <asim.samuel1@gmail.com>
2020-08-18 16:03:54 -0400

    Feat: completed add two numbers with more readability, my favorite solution :)

--- LinkedLists/addTwoNumbers.js
+++ LinkedLists/addTwoNumbers.js
@@ -8,10 +8,12 @@ class ListNode {
 const addTwoNumbers = (l1, l2) => {
   let list = new ListNode()
   let currHead = list
+  //carry must be either 0 or 1 because the largest possible sum of two digits (including the carry) is 9 + 9 + 1 = 19
   let carry = 0
 
   while (l1 || l2) {
     let num = 0
+    //add current values from each list to
     if (l1) num += l1.val
     if (l2) num += l2.val
     if (carry) num += carry
@@ -35,4 +37,18 @@ const addTwoNumbers = (l1, l2) => {
   }
   currHead.next = null
   return list
+}
+//--------------------------------------------------------
+///Recursive Solution
+const addTwoNums = (l1, l2) => {
+  const result = new ListNode();
+
+  function traverse(n1, n2, sum, carry) {
+    if (!n1 && !n2 && !carry) return result.next;
+    let val = (n1 && n1.val) + (n2 && n2.val) + carry;
+    carry = val > 9;
+    sum.next = new ListNode(val % 10);
+    return traverse((n1 && n1.next), (n2 && n2.next), sum.next, carry);
+  }
+  return traverse(l1, l2, result, 0);
 }
