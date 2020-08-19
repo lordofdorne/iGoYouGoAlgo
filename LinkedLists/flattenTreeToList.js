cc8af05d4d742b110bcd5d452a5931dda4c0c192
asim samuel <asim.samuel1@gmail.com>
2020-08-19 17:07:47 -0400

    Chore: Completed flatten binary tree to linked list

---
+++ LinkedLists/flattenTreeToList.js
@@ -0,0 +1,24 @@
+const flattenTree = (root) => {
+  if (!root) return root
+
+  let helper = (node, stack = []) => {
+    //push the node to the stack
+    stack.push(node)
+    //check for children if they exist add them to stack
+    if (node.left) helper(node.left, stack)
+    if (node.right) helper(node.right, stack)
+    return arr
+  }
+
+  //insert the tree into the helper function
+  let nodes = helper(root)
+  //loop through the stack
+  for (let i = 0; i < nodes.length - 1; i++) {
+    let currNode = nodes[i]
+    //the key is to set every nodes left property to null and force elements to the right
+    currNode.left = null
+    currNode.right = nodes[i + 1]
+  }
+  //return from the starting index
+  return nodes[0]
+}
