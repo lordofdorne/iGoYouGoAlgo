63c1ef45e73ee19da92472ba9fabca2116b8e620
asim samuel <asim.samuel1@gmail.com>
2020-08-25 10:58:45 -0400

    Feat: updated remove nth node

--- LinkedLists/removeNthNode.js
+++ LinkedLists/removeNthNode.js
@@ -4,6 +4,9 @@
 // Given linked list: 1->2->3->4->5, and n = 2.
 
 // After removing the second node from the end, the linked list becomes 1->2->3->5.function ListNode(val) {
+
+//Time Complexity is O(n) where n is the length of the linked list
+//Space Complexity is constant O(1) because we're just keeping track of nodes
 function ListNode(val) {
   this.val = val;
   this.next = null;
@@ -32,5 +35,33 @@ const removeNthNodeFromEnd = (head, n) => {
   }
   slow.next = slow.next.next
   return dummy.next
+
+}
+
+////----------------------------------
+
+removeNthNodeFromEnd = (head, n) => {
+  let counter = 1
+  let slow = head
+  let fast = head
+
+  //move the fast pointer to n nodes ahead
+  while (counter <= n) {
+    fast = fast.next
+    counter++
+  }
+  //if we get to the end that means the first pointer is the one that we need to remove
+  if (fast === null) {
+    head.value = head.next.value
+    head.next = head.next.next
+    return
+  }
+  //move both pointers at the same time
+  while (fast.next !== null) {
+    slow = slow.next
+    fast = fast.next
+  }
+  slow.next = slow.next.next
+
 
 }
