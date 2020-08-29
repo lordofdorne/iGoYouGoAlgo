3d5fa70f6be60a4acbf5e38db94268e4eb87bd2c
asim samuel <asim.samuel1@gmail.com>
2020-08-29 13:49:34 -0400

    Chore: completed max ara of island

---
+++ Graphs/maxAreaOfIsland.js
@@ -0,0 +1,27 @@
+// Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+// Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+const maxAreaOfIsland = grid => {
+  const res = { count: 0 }
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      dfs(grid, r, c, res)
+    }
+  }
+  return res.count
+}
+
+
+//dfs helper
+const dfs = (grid, r, c, res, area = { count: 0 }) => {
+  if (!grid[r] || !grid[r][c]) return
+
+  res.count = Math.max(res.count, area.count += grid[r][c])
+  grid[r][c] = 0
+
+  dfs(grid, r + 1, c, res, area)
+  dfs(grid, r - 1, c, res, area)
+  dfs(grid, r, c + 1, res, area)
+  dfs(grid, r, c - 1, res, area)
+}
