c0f54555b0d65fe3275771711302298332e23d74
asim samuel <asim.samuel1@gmail.com>
2020-09-05 00:42:33 -0400

    Chore: completed closest value in BST

---
+++ Trees/closestValueInBST.js
@@ -0,0 +1,30 @@
+//write a funciton that takes in a BST and a target integer value and returns the closest value to that target value contained in the BST
+
+function findClosestValueInBst(tree, target) {
+  return findClosestValue(tree, target, tree.value)
+}
+
+function findClosestValue(tree, target, closest) {
+  if (tree === null) return closest
+
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+    closest = tree.value
+  }
+
+  if (target < tree.value) {
+    return findClosestValue(tree.left, target, closest)
+  } else if (target > tree.value) {
+    return findClosestValue(tree.right, target, closest)
+  } else {
+    return closest
+  }
+}
+
+// This is the class of the input tree. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
