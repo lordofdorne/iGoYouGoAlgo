f0d9764f71cb1fbeb4ef1e7cd24ccda75a044c03
asim samuel <asim.samuel1@gmail.com>
2020-09-10 00:00:47 -0400

    Chore: completed first unique string

---
+++ Strings/firstUniqueChar.js
@@ -0,0 +1,36 @@
+// Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+// Examples:
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode"
+// return 2.
+
+
+// var firstUniqChar = function(s) {
+//   const map = {};
+
+//   for(let i = 0; i < s.length; i++) {
+//       map[s[i]] = (map[s[i]] || 0) + 1;
+//   }
+
+//   for(let i = 0; i < s.length; i++) {
+//       if(map[s[i]] === 1) return i;
+//   }
+//   return -1;
+// };
+
+const firstUniqeChar = s => {
+  const map = {};
+
+  for (let i = 0; i < s.length; i++) {
+    map[s[i]] = (map[s[i]] || 0) + 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] === 1) return i;
+  }
+  return -1;
+}
