487ea0e678af070b8cb946e0cb0ae454fe1412aa
Asim Samuel <asim.samuel1@gmail.com>
2023-12-25 21:06:13 -0500

    completed top k frquent

---
+++ Arrays/top_k_frequent.py
@@ -0,0 +1,38 @@
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+# k is in the range [1, the number of unique elements in the array].
+# It is guaranteed that the answer is unique.
+
+def topKFrequent(nums, k):
+    
+    hashmap = {}
+    bucket = [[] for i in range(len(nums) + 1)]
+
+    for n in nums:
+        hashmap[n] = 1 + hashmap.get(n, 0)
+
+    for n,c in hashmap.items():
+        bucket[c].append(n)
+    
+    res = []
+    for i in range(len(bucket) - 1, 0, -1):
+        for n in bucket[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
