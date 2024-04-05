5b967094127a378aa4c3b80b4042d10cec7b4ae5
Asim Samuel <asim.samuel1@gmail.com>
2024-04-05 17:16:56 -0400

    added convert bst to dll

--- Trees/convert_bst_to_dll.py
+++ Trees/convert_bst_to_dll.py
@@ -1,9 +1,3 @@
-# Convert a Binary Search Tree to a sorted Circular Doubly-Linked List in place.
-
-# You can think of the left and right pointers as synonymous to the predecessor and successor pointers in a doubly-linked list. For a circular doubly linked list, the predecessor of the first element is the last element, and the successor of the last element is the first element.
-
-# We want to do the transformation in place. After the transformation, the left pointer of the tree node should point to its predecessor, and the right pointer should point to its successor. You should return the pointer to the smallest element of the linked list.
-
 """
 # Definition for a Node.
 class Node:
@@ -12,15 +6,30 @@ class Node:
         self.left = left
         self.right = right
 """
+
 class Solution:
-    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+    def treeToDoublyList(self, root):
         if not root:
             return None
         
         self.first = None
         self.last = None
-
-        self.inorder_link(root)
+        self.inorder(root)
         self.first.left = self.last
         self.last.right = self.first
         return self.first
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+        
+            if not self.last:
+                self.first = node
+            else:
+                node.left = self.last
+                self.last.right = node
+            self.last = node
+            self.inorder(node.right)
+
+# Time:O(n)
+# Space:O(n)
