30a53490cee7df9bbb09e8a8d7d73918a7049588
Asim Samuel <asim.samuel1@gmail.com>
2024-04-06 19:29:30 -0400

    completed intervl intersection

---
+++ Arrays/interval_list_sections.py
@@ -0,0 +1,31 @@
+# You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+
+# Return the intersection of these two interval lists.
+
+# A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+
+# The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+from typing import List
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        p1 = 0
+        p2 = 0
+        res = []
+
+        while p1 < len (firstList) and p2 < len(secondList):
+            start1, end1 = firstList[p1]
+            start2, end2 = secondList[p2]
+
+            if start1 > end2:
+                p2 += 1
+            elif start2 > end1:
+                p1 += 1
+            else:
+                res.append([max(start1, start2), min(end1, end2)])
+
+                if end1> end2:
+                    p2 +=1
+                else:
+                    p1 += 1
+        return res
